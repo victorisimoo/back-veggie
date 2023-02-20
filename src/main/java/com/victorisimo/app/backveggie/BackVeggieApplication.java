2f63d49ad2000dce1cbe009565bb993f9550cf48
Victor Hernández <me@victorisimo.com>
2023-02-19 22:30:03 -0600

    first commit veggie back-end

---
+++ src/main/java/com/victorisimo/app/backveggie/BackVeggieApplication.java
@@ -0,0 +1,13 @@
+package com.victorisimo.app.backveggie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackVeggieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackVeggieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/victorisimo/app/backveggie/BackVeggieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorisimo.app.backveggie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackVeggieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
